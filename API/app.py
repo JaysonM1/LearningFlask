4306eef7f2d5bdec8801b4f76fadb73467f1e6ff
Jayson <jaysonm@udel.edu>
2023-03-15 22:54:42 -0400

    new put,del endpoints

--- API/app.py
+++ API/app.py
@@ -33,7 +33,7 @@ def create_item():
     item_data = request.get_json()
     if ("price" not in item_data or "store_id" not in item_data or "name" not in item_data):
         abort(400, message = "Bad request. Ensure price store_id and name are include in the JSON payload")
-    for item in item.values():
+    for item in items.values():
         if(item_data["name"] == item["name"] and item_data["store_id"] == item["store_id"]):
             abort(400, message = f"Item already exists.")
     if item_data["store_id"] not in stores:
@@ -65,3 +65,33 @@ def get_item(item_id):
         return items[item_id]
     except:
         abort(404, message = "Item not found.")
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try: 
+        del items[item_id]
+        return {"message": "Item Deleted"}
+    except:
+        abort(404, message = "Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message = "Bed request. Ensure 'price' and 'name are included in the JSON payload")
+    try:
+        item = items[item_id]
+        item |= item_data
+    except KeyError:
+        abort(404, message = "Item not found")
+
+
+
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try: 
+        del stores[store_id]
+        return {"message": "Store Deleted"}
+    except:
+        abort(404, message = "Store not found.")
